0163c80a7c9f3955685f86646f5ece899be14304
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2014-06-15 14:42:32 -0400

    Successful refactoring of article.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,7 +6,7 @@
 
 function populateFrontpage() {
   // Do this for every url in the interests
-  url = "http://www.npr.org/rss/rss.php?id=1001";
+  url = 'http://rss.nytimes.com/services/xml/rss/nyt/World.xml';
   var feedItem = loadFeed(url);
   var articleModel = new ArticleModel(feedItem);
   var articleView = new ArticleView(articleModel);
