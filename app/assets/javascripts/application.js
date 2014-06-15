1a801c734a5b61415d3260e1c636b227c992fa12
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2014-06-15 12:10:18 -0400

    Move app.js to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,17 +1,17 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
 
+function populateFrontpage() {
+  url = "http://www.npr.org/rss/rss.php?id=1001";
+  var feedItem     = loadFeed(url);
+  var articleModel = new ArticleModel(feedItem);
+  var articleView  = new ArticleView(articleModel);
+  $('.frontpage').append(articleView.render().el);
+}
+
+$(function(){
+  populateFrontpage();
+  //feedItems();
+})
--- app/assets/javascripts/article.js
+++ app/assets/javascripts/article.js
@@ -1,8 +1,8 @@
-function feedItems(){
-var feed = new google.feeds.Feed("http://www.npr.org/rss/rss.php?id=1001");
-feed.setResultFormat(google.feeds.Feed.JSON_FORMAT);
-feed.setNumEntries(1)
-return feed;
+function loadFeed(url){
+  var feed = new google.feeds.Feed(url);
+  feed.setResultFormat(google.feeds.Feed.JSON_FORMAT);
+  feed.setNumEntries(1)
+  return feed;
 }
 
 //******Model*******
@@ -31,17 +31,15 @@ function ArticleView(model){
   this.el = undefined;
 }
 
-ArticleView.prototype.render = function() {
-  //var div = $('<div>');
-  function populateFrontpage(result) {
-    i = 0;
-    for (var i = 0; i < result.feed.entries.length; i++) {
+ArticleView.prototype.render = function(result) {
+  for (var i = 0; i < result.feed.entries.length; i++) {
+    var $div = $('<div>');  
     $div = $('div').addClass('article')
-    $div.append('<div class = "date">' + result.feed.entries[i].publishedDate + '</div>');
+    $div.append('<div class="date">' + result.feed.entries[i].publishedDate + '</div>');
     $div.append('<a href="'+ result.feed.entries[i].link +'">' + result.feed.entries[i].title + '</a>');
-    $div.append('<div class = "extract">' + result.feed.entries[i].content + '</div>');
-    }
-    $('.frontpage').append($div)
+    $div.append('<div class="extract">' + result.feed.entries[i].content + '</div>');
   }
   this.model.feed.load(populateFrontpage);
+  this.el = $div;
+  return this; 
 };
