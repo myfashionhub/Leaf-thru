65cdda6694689730de87e94ea1e24b80ae96895a
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2014-06-16 16:52:22 -0400

    Merge conflict in readers controller

--- app/controllers/readers_controller.rb
+++ app/controllers/readers_controller.rb
@@ -43,20 +43,6 @@ class ReadersController < ApplicationController
       config.access_token_secret = current_reader.twitter_token_secret
     end
 
-<<<<<<< HEAD
-    tweets = client.home_timeline(options={count: 50})
-    links  = Reader.twitter_feed(tweets)
-    @data  = Article.parse(links)
-  end
-
-  def facebook
-    graph = Koala::Facebook::API.new(current_reader.facebook_token, ENV['FACEBOOK_SECRET'])
-    @reader = current_reader
-    #render :json => feed.to_json
-  end 
-
-  def feed
-=======
     tweets    = client.home_timeline(options={count: 10})
     links     = Reader.twitter_feed(tweets)
     @articles = Article.parse(links)
@@ -74,17 +60,10 @@ class ReadersController < ApplicationController
 
   def feed
     #@feeds = current_reader.interests.all
-
->>>>>>> 84aef86727a9f8443bd6ac6e992193f6ae8bc6d5
   end
 
   private
-  def params_reader
-<<<<<<< HEAD
-    params.require(:reader).permit(:email, :password, :location)
-=======
     params.require(:reader).permit(:email, :password)
->>>>>>> 84aef86727a9f8443bd6ac6e992193f6ae8bc6d5
   end
 
 end
