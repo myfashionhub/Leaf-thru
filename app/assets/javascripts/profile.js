381a90fa383605b8e9838cc1eeb264a59b4b3633
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2014-07-20 12:48:20 -0400

    Add/remove subscriptions by dragging & dropping

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -28,6 +28,7 @@ function togglePublications() {
   var firstCat = $('.categories').children().first();
   firstCat.find('h4').addClass('current');
   firstCat.find('ul').clone().appendTo($('.publications')).show();
+  subscriptionBuilder();
 
   $('.category h4').click(function(e) {
     $('.current').removeClass('current');
@@ -35,15 +36,17 @@ function togglePublications() {
     var pubUl = $(e.target).parent().find('ul');
     $('.publications').empty();
     pubUl.clone().appendTo($('.publications')).hide().toggle('fold');
+    subscriptionBuilder();
   });
+  subscriptionBuilder();
 }
 
 
 function updateSubscription() {
-  var checkboxes = $('input:checked');
+  var pubs    = $('.current-sub li');
   var pub_ids = [];
-  for (var i = 0; i < checkboxes.length; i++) {
-    pub_ids.push($(checkboxes[i]).val());
+  for (var i = 0; i < pubs.length; i++) {
+    pub_ids.push($(pubs[i]).attr('data'));
   }
 
   $.ajax({
@@ -65,12 +68,15 @@ function currentSubscription() {
     dataType: 'json',
     success: function(data) {
       for (i = 0; i < data.length; i++) {
-        var pub = $('<li>').html(data[i].name);
+        var pub = $('<li>').html(data[i].name).attr('data', data[i].id);
+        $(pub).append('<i class="fa fa-times"></i>');
         $('.current-sub ul').append(pub);
+        $('i').click(function(e) {
+          $(e.target).parent().remove();
+        });
       }
     }
   });
-
 }
 
 function updateProfile() {
@@ -89,4 +95,25 @@ function updateProfile() {
       notify();
     }
   })
+}
+
+function subscriptionBuilder() {
+  var pubs = $('.publications li');
+  pubs.draggable({ cursor: 'move', helper: 'clone' });
+
+  $('.current-sub').droppable({
+    drop: function(e, dropped) {
+      var pubLi = dropped.draggable;
+      $(pubLi).append('<i class="fa fa-times"></i>');
+      $(pubLi).appendTo($('.current-sub ul'));
+      removeSubscription();
+    }
+  });
+}
+
+function removeSubscription() {
+  $('i').click(function(e) {
+    var pubLi = $(e.target).parent();
+    pubLi.remove();
+  });
 }
