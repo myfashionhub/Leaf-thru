706c553eeb74c1b41be3909937abfd18ac7e00fe
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2015-08-19 01:13:00 +0200

    Not err when updating empty subscription

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -9,15 +9,8 @@ class SubscriptionsController < ApplicationController
   end
 
   def create
-    Subscription.destroy_all(:reader_id => current_reader.id)
-    #clear out all subs to prevent keeping feeds readers don't want
-    pub_ids = params[:pub_ids].uniq
-    pub_ids.each do |pub_id|
-      Subscription.create({
-       reader_id: current_reader.id,
-       publication_id: pub_id
-      })
-    end
+    Subscription.update(params, current_reader)
+
     render json: { msg: "You have successfully updated your subscriptions." }.to_json
   end
 
