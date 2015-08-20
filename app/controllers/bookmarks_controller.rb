e574dd210940fe8aac3ea0a96e04962d30538d57
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2015-08-20 14:34:34 +0200

    Add rescue for publications that no longer exist

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,31 +1,9 @@
 class BookmarksController < ApplicationController
+  
   def index
     readers = Reader.all.to_a
     readers.keep_if { |reader| reader.id != current_reader.id }
-
-    @leafers = readers.map do |reader|
-      if reader.name
-        reader_info = reader.name
-      else
-        reader_info = reader.email
-      end
-
-      subscriptions = Subscription.where(reader_id: reader.id)
-      publications = subscriptions.map do |subscription|
-        Publication.find(subscription.publication_id)
-      end
-      bookmarks = Bookmark.where(reader_id: reader.id).to_a
-      articles  = bookmarks.map do |bookmark|
-        Article.find(bookmark.article_id)
-      end
-
-      shared_articles = articles.keep_if do |article|
-        Bookmark.find_by(article_id: article.id, reader_id: current_reader.id) != nil
-      end
-
-      { reader_info: reader_info, reader_image: reader.image, 
-        publications: publications, shared_articles: shared_articles
-      }
-    end
+    @leafers = Bookmark.retrieve_subscription_info(readers, current_reader)
   end
+
 end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,37 @@
 class Bookmark < ActiveRecord::Base
   belongs_to :readers
   belongs_to :articles
+
+  def self.retrieve_subscription_info(readers, current_reader)
+    readers.map do |reader|
+      if reader.name
+        reader_info = reader.name
+      else
+        reader_info = reader.email
+      end
+
+      subscriptions = Subscription.where(reader_id: reader.id)
+      publications = subscriptions.map do |subscription|
+        Publication.find(subscription.publication_id) rescue nil
+      end
+
+      bookmarks = Bookmark.where(reader_id: reader.id).to_a
+      articles  = bookmarks.map do |bookmark|
+        Article.find(bookmark.article_id)
+      end
+
+      shared_articles = articles.keep_if do |article|
+        Bookmark.find_by(
+          article_id: article.id, 
+          reader_id: current_reader.id
+        ) != nil
+      end
+
+      { reader_info: reader_info, 
+        reader_image: reader.image, 
+        publications: publications.compact, 
+        shared_articles: shared_articles
+      }
+    end
+  end   
 end
