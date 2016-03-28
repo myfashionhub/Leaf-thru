f428ace8c4b03d7c52ac79cc35b2d693f724a8bd
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2016-03-27 20:26:19 -0400

    Refactor Alchemy + Twitter modules

--- lib/alchemy.rb
+++ lib/alchemy.rb
@@ -1,26 +1,31 @@
 module Alchemy
+
   def self.get_articles(links)
+    alchemy_endpoint = "http://access.alchemyapi.com/calls/url"
+    api_key          = ENV['LT_ALCHEMY_KEY']
+
     # Queue up parse article requests
     hydra = Typhoeus::Hydra.new
+
     requests = links.map do |link|
-      apikey      = ENV['LT_ALCHEMY_KEY']
-      url         = link[:url]
-      query       = "?apikey=#{apikey}&url=#{url}&outputMode=json"
-      title_query = "http://access.alchemyapi.com/calls/url/URLGetTitle"+query
-      text_query  = "http://access.alchemyapi.com/calls/url/URLGetText"+query
+      query       = "?apikey=#{api_key}&url=#{link[:url]}&outputMode=json"
+      title_query = alchemy_endpoint + "/URLGetTitle" + query
+      text_query  = alchemy_endpoint + "/URLGetText" + query
       title_request = Typhoeus::Request.new(title_query, followlocation: true)
       text_request  = Typhoeus::Request.new(text_query, followlocation: true)
       hydra.queue(title_request)
       hydra.queue(text_request)
 
-      { link: link,
+      {
+        link: link,
         title_request: title_request,
         text_request: text_request
       }
     end
-
     hydra.run
-    return requests
+
+    articles = self.parse_responses(requests)
+    self.filter_articles(articles)
   end
 
   def self.parse_responses(requests)
@@ -45,11 +50,13 @@ module Alchemy
       url       = title['url'] || link[:url]
       shared_by = link[:shared_by]
 
-    { title: title, url: url,
-      extract: extract, shared_by: shared_by
-    }
+      {
+        title: title,
+        url: url,
+        extract: extract,
+        shared_by: shared_by
+      }
     end
-
   end
 
   def self.filter_articles(articles)
--- lib/twitter.rb
+++ lib/twitter.rb
@@ -1,40 +1,48 @@
 module Twitter
-  def self.get_feed(token, token_secret)
-    client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV['LT_TWITTER_KEY']
-      config.consumer_secret     = ENV['LT_TWITTER_SECRET']
-      config.access_token        = token
-      config.access_token_secret = token_secret
-    end
+  class Feed
+    attr_reader  :client
 
-    tweets = client.home_timeline(options={count: 15, include_entities: true})
-  end
-
-  def self.collect_links(tweets)
-    links = tweets.map do |tweet|
-      url    = tweet.urls[0]
-      if url.nil? == false
-        url       = url.attrs[:expanded_url]
-        shared_by = tweet.user.screen_name
-        { url: url, shared_by: shared_by }
+    def initialize(token, token_secret)
+      @client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV['LT_TWITTER_KEY']
+        config.consumer_secret     = ENV['LT_TWITTER_SECRET']
+        config.access_token        = token
+        config.access_token_secret = token_secret
       end
     end
-  end
 
-  def self.filter_sources(links)
-    links.compact.delete_if { |link| #regex domain is in?
-      link[:url].empty? ||
-      link[:url].include?('youtu.be') ||
-      link[:url].include?('youtube.com') ||
-      link[:url].include?('pinterest.com') ||
-      link[:url].include?('pin.it') ||
-      link[:url].include?('vimeo.com') ||
-      link[:url].include?('vine.co') ||
-      link[:url].include?('twitpic.com') ||
-      link[:url].include?('instagram.com') ||
-      link[:url].include?('login') ||
-      link[:url].include?('shop')
-    }
-  end
+    def get_timeline
+      tweets = client.home_timeline(options={count: 15, include_entities: true})
+      links  = collect_links(tweets)
+      article_links = filter_sources(links)
+    end
 
+    def collect_links(tweets)
+      links = tweets.map do |tweet|
+        url    = tweet.urls[0]
+        if url.nil? == false
+          url       = url.attrs[:expanded_url]
+          shared_by = tweet.user.screen_name
+          { url: url, shared_by: shared_by }
+        end
+      end
+    end
+
+    def filter_sources(links)
+      links.compact.delete_if { |link| #regex domain is in?
+        link[:url].empty? ||
+        link[:url].include?('youtu.be') ||
+        link[:url].include?('youtube.com') ||
+        link[:url].include?('pinterest.com') ||
+        link[:url].include?('pin.it') ||
+        link[:url].include?('vimeo.com') ||
+        link[:url].include?('vine.co') ||
+        link[:url].include?('twitpic.com') ||
+        link[:url].include?('instagram.com') ||
+        link[:url].include?('login') ||
+        link[:url].include?('shop')
+      }
+    end
+
+  end
 end
