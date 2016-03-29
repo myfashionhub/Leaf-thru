91db757338bbaa33ee6e5e5cf672bdb591f1d629
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2016-03-28 22:26:54 -0400

    Refactor alchemy module

--- lib/alchemy.rb
+++ lib/alchemy.rb
@@ -1,18 +1,18 @@
 module Alchemy
 
   def self.get_articles(links)
-    alchemy_endpoint = "http://access.alchemyapi.com/calls/url"
-    api_key          = ENV['LT_ALCHEMY_KEY']
-
-    # Queue up parse article requests
+    alchemy_url = "http://access.alchemyapi.com/calls/url"
+    api_key     = ENV['LT_ALCHEMY_KEY']
     hydra = Typhoeus::Hydra.new
 
     requests = links.map do |link|
       query       = "?apikey=#{api_key}&url=#{link[:url]}&outputMode=json"
-      title_query = alchemy_endpoint + "/URLGetTitle" + query
-      text_query  = alchemy_endpoint + "/URLGetText" + query
+      title_query = alchemy_url + "/URLGetTitle" + query
+      text_query  = alchemy_url + "/URLGetText" + query
       title_request = Typhoeus::Request.new(title_query, followlocation: true)
       text_request  = Typhoeus::Request.new(text_query, followlocation: true)
+
+      # Queue up parse article requests
       hydra.queue(title_request)
       hydra.queue(text_request)
 
@@ -24,46 +24,43 @@ module Alchemy
     end
     hydra.run
 
-    articles = self.parse_responses(requests)
-    self.filter_articles(articles)
+    parse_responses(requests)
   end
 
   def self.parse_responses(requests)
     # Parse responses from hydra requests
-    articles = requests.map do |response|
-      link = response[:link]
-      title_request = response[:title_request]
-      text_request = response[:text_request]
+    articles = requests.map do |res|
+      link  = res[:link]
+      title = JSON.parse(res[:title_request].response.response_body)
+      text  = JSON.parse(res[:text_request].response.response_body)
 
-      title = JSON.parse(title_request.response.response_body)
-      text  = JSON.parse(text_request.response.response_body)['text']
-
-      if !text.nil?
-        text_end = text.index(/\n/).to_i
-        text_end = text.index('.').to_i + 1 if text_end <= 60
-        extract   = text[0, text_end]
-      else
-        extract = ''
-      end
-
-      title     = title['title'] || ''
+      title     = title['title']
       url       = title['url'] || link[:url]
-      shared_by = link[:shared_by]
+      extract   = get_extract(text['text'])
 
-      {
-        title: title,
-        url: url,
-        extract: extract,
-        shared_by: shared_by
-      }
+      if url.empty? || title.empty? || extract.length <= 30
+        nil
+      else
+        {
+          title: title,
+          url: url,
+          extract: extract,
+          shared_by: link[:shared_by]
+        }
+      end
     end
+
+    articles.compact
   end
 
-  def self.filter_articles(articles)
-    articles.delete_if do |article|
-      article[:url].empty? || article[:title].empty? ||
-      article[:extract].length <= 30
+  def self.get_extract(text)
+    extract = ''
+    if text.present?
+      text_end = text.index(/\n/).to_i
+      text_end = text.index('.').to_i + 1 if text_end <= 60
+      extract  = text[0, text_end]
     end
-  end
 
+    extract
+  end
 end
