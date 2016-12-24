a6337675e402ce5d9f52effeabfcb0c4119339cf
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2016-12-24 14:05:16 -0500

    Cache feed responses

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -6,15 +6,25 @@ class FeedsController < ApplicationController
   end
 
   def twitter
-    articles = Feed.twitter(
-                 current_reader.twitter_token,
-                 current_reader.twitter_token_secret
-               )
+    articles = Rails.cache.fetch(
+      "/#{current_reader.id}/twitter", expires_in: 2.hours
+    ) do
+      Feed.twitter(
+        current_reader.twitter_token,
+        current_reader.twitter_token_secret
+      )
+    end
+
     render json: articles.to_json
   end
 
   def rss
-    articles = Feed.rss(current_reader.subscriptions)
+    articles = Rails.cache.fetch(
+      "/#{current_reader.id}/rss", expires_in: 2.hours
+    ) do
+      Feed.rss(current_reader.subscriptions)
+    end
+
     render json: articles.to_json
   end
 
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -5,6 +5,7 @@ class Feed < ActiveRecord::Base
   def self.twitter(token, token_secret)
     article_links = Twitter::Feed.new(token, token_secret).get_timeline
     articles = Alchemy.get_articles(article_links)
+    articles.length > 0 ? articles : nil
   end
 
   def self.rss(subscriptions)
