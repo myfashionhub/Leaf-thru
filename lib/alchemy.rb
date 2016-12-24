afa8773ba2e4e1869d7fcd57b22ba4a24ff9f963
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2016-12-24 13:57:52 -0500

    Use Alchemy API combined data endpoint

--- lib/alchemy.rb
+++ lib/alchemy.rb
@@ -1,51 +1,54 @@
+require 'net/http'
+require 'uri'
+
 module Alchemy
 
   def self.get_articles(links)
-    alchemy_url = "https://access.alchemyapi.com/calls/url"
+    alchemy_url = "https://gateway-a.watsonplatform.net/calls/url/URLGetCombinedData"
     api_key     = ENV['LT_ALCHEMY_KEY']
     hydra = Typhoeus::Hydra.new
 
     requests = links.map do |link|
-      query       = "?apikey=#{api_key}&url=#{link[:url]}&outputMode=json"
-      title_query = alchemy_url + "/URLGetTitle" + query
-      text_query  = alchemy_url + "/URLGetText" + query
-      title_request = Typhoeus::Request.new(title_query, followlocation: true)
-      text_request  = Typhoeus::Request.new(text_query, followlocation: true)
+      # Attempt to get redirect URL of short links
+      url = Net::HTTP.get_response( URI(link[:url]) )['location']
 
-      # Queue up parse article requests
-      hydra.queue(title_request)
-      hydra.queue(text_request)
+      query = "#{alchemy_url}?apikey=#{api_key}&url=#{url}" +
+        "&outputMode=json&extract=title&showSourceText=1&sourceText=cleaned"
+      request = Typhoeus::Request.new(query, followlocation: true)
+      hydra.queue(request)
 
       {
         link: link,
-        title_request: title_request,
-        text_request: text_request
+        request: request
       }
     end
-    hydra.run
 
+    hydra.run # Execute all queued requests
     parse_responses(requests)
   end
 
   def self.parse_responses(requests)
-    # Parse responses from hydra requests
     articles = requests.map do |res|
-      link  = res[:link]
-      title = JSON.parse(res[:title_request].response.response_body)
-      text  = JSON.parse(res[:text_request].response.response_body)
+      status = res[:request].response.options[:response_code]
+      return nil if status != 200
 
-      title     = title['title']
-      url       = title.present? ? title['url'] : link[:url]
-      extract   = get_extract(text['text'])
+      begin
+        response = JSON.parse(res[:request].response.response_body)
+      rescue # In case response body isn't JSON
+        return nil
+      end
 
-      if url.empty? || title.nil? || title.empty? || extract.length <= 30
+      title   = response['title']
+      extract = get_extract(response['text'])
+
+      if !title.present? || extract.length <= 30
         nil
       else
         {
           title: title,
-          url: url,
+          url: res[:link][:url],
           extract: extract,
-          shared_by: link[:shared_by]
+          shared_by: res[:link][:shared_by]
         }
       end
     end
@@ -55,10 +58,18 @@ module Alchemy
 
   def self.get_extract(text)
     extract = ''
-    if text.present?
-      text_end = text.index(/\n/).to_i
-      text_end = text.index('.').to_i + 1 if text_end <= 60
-      extract  = text[0, text_end]
+    return extract if !text.present?
+
+    # Get first paragraph or some sentences, limit 250 characters
+    text.strip!
+    text_end = text.index(/\n/).to_i
+    if text_end <= 250
+      extract = text[0, text_end]
+    else
+      text.gsub('\n', ' ').split('. ').each do |sentence|
+        sentence = sentence + '. '
+        extract += sentence if (extract + sentence).length <= 250
+      end
     end
 
     extract
